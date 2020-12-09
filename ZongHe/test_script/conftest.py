c4171c87dc40e4d8e5c678bdc3d594eef2c44b42
yangchenchen18 <75718284+yangchenchen18@users.noreply.github.com>
2020-12-09 19:19:25 +0800

    Add files via upload

--- ZongHe/test_script/conftest.py
+++ ZongHe/test_script/conftest.py
@@ -1,11 +1,31 @@
 '''
 脚本层的一些公关方法
 '''
+import os
+import sys
+
 import pytest
+
 
 from ZongHe.caw import DataRead
 from ZongHe.caw.BaseRequests import BaseRequests
 
+def get_project_path():
+    '''
+    获取工程路径
+    :return: 当前工程路径，比如E:\ApiAutoTest\ZongHe\
+    '''
+    # __file__ 存储着当前文件的路径
+    path = os.path.realpath(__file__)
+    # 上一级目录
+    path = os.path.dirname(path)
+    # 再上一级目录
+    path = os.path.dirname(path)
+    path = os.path.dirname(path)
+    return path + "\\"
+
+print(get_project_path())
+sys.path.append(get_project_path())
 
 @pytest.fixture(scope="session")
 def url():
